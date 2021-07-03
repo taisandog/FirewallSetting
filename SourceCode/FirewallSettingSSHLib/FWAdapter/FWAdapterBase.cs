6e5855a721e58dad3b105db36d09568584e019f2
taisandog@gmail.com <taisandog@gmail.com>
2021-07-04 00:59:29 +0800

    增加适配器以兼容多种防火墙

---
+++ SourceCode/FirewallSettingSSHLib/FWAdapter/FWAdapterBase.cs
@@ -0,0 +1,165 @@
+﻿using Renci.SshNet;
+using SettingLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirewallSettingSSHLib.FWAdapter
+{
+    /// <summary>
+    /// 防火墙操作适配器基类
+    /// </summary>
+    public abstract class FWAdapterBase
+    {
+
+
+        protected List<FirewallItem> _firewallRule;
+        /// <summary>
+        /// 防火墙规则名
+        /// </summary>
+        public List<FirewallItem> FirewallRule
+        {
+            get
+            {
+                return _firewallRule;
+            }
+            set
+            {
+                _firewallRule = value;
+            }
+        }
+
+        protected List<FWUser> _allUser;
+
+        /// <summary>
+        /// 所有用户
+        /// </summary>
+        public List<FWUser> AllUser
+        {
+            get
+            {
+                return _allUser;
+            }
+            set 
+            {
+                _allUser = value;
+            }
+        }
+
+        public abstract string Name { get; }
+
+        /// <summary>
+        /// 检查是否开启
+        /// </summary>
+        /// <param name="ssh"></param>
+        /// <returns></returns>
+        public abstract bool CheckEnable(SshClient ssh);
+        /// <summary>
+        /// 生成要执行的指令
+        /// </summary>
+        /// <param name="ssh"></param>
+        /// <returns></returns>
+        public abstract List<string> CreateCommand(SshClient ssh);
+        /// <summary>
+        /// 重新加载防火墙
+        /// </summary>
+        /// <param name="ssh"></param>
+        public abstract void ReLoad(SshClient ssh);
+        /// <summary>
+        /// 加载规则端口
+        /// </summary>
+        /// <returns></returns>
+        protected virtual Dictionary<int, bool> LoadRulePort()
+        {
+            //加载接管的端口
+            Dictionary<int, bool> dicPort = new Dictionary<int, bool>();
+            foreach (FirewallItem item in _firewallRule)
+            {
+                dicPort[item.Port] = true;
+            }
+            return dicPort;
+        }
+
+        /// <summary>
+        /// 加载用户IP
+        /// </summary>
+        /// <returns></returns>
+        public virtual List<string> LoadUserIP()
+        {
+            string lanIP = System.Configuration.ConfigurationManager.AppSettings["Server.AllowIP"];
+            Dictionary<string, bool> dicExists = new Dictionary<string, bool>();
+            List<string> lstIP = new List<string>(_allUser.Count);
+            List<string> cur = null;
+            if (!string.IsNullOrWhiteSpace(lanIP))
+            {
+                lstIP.Add(lanIP);
+            }
+            foreach (FWUser user in _allUser)
+            {
+                cur = user.GetIP();
+                foreach (string ip in cur)
+                {
+                    if (string.IsNullOrWhiteSpace(ip))
+                    {
+                        continue;
+                    }
+                    if (dicExists.ContainsKey(ip))
+                    {
+                        continue;
+                    }
+                    lstIP.Add(ip);
+                    dicExists[ip] = true;
+                }
+            }
+            return lstIP;
+        }
+
+        /// <summary>
+        /// 获取键
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="sport"></param>
+        /// <returns></returns>
+        protected virtual string GetKey(string ip, int port, string protocol)
+        {
+            StringBuilder sbRet = new StringBuilder();
+            sbRet.Append(ip);
+            sbRet.Append("_");
+            sbRet.Append(port.ToString());
+            sbRet.Append("_");
+            sbRet.Append(protocol);
+            return sbRet.ToString();
+        }
+
+
+        /// <summary>
+        /// 截取内容
+        /// </summary>
+        /// <param name="tag">标记</param>
+        /// <param name="line">内容</param>
+        /// <returns></returns>
+        protected virtual string SubValue(string tag, string line)
+        {
+            int startindex = line.IndexOf(tag, StringComparison.CurrentCultureIgnoreCase);
+            if (startindex < 0)
+            {
+                return null;
+            }
+            startindex = line.IndexOf("\"", startindex + 1, StringComparison.CurrentCultureIgnoreCase) + 1;
+            if (startindex < 0)
+            {
+                return null;
+            }
+
+            int endindex = line.IndexOf("\"", startindex + 1, StringComparison.CurrentCultureIgnoreCase);
+            if (endindex < 0)
+            {
+                return null;
+            }
+            return line.Substring(startindex, endindex - startindex);
+        }
+
+    }
+}
---
+++ SourceCode/FirewallSettingSSHLib/FWAdapter/FirewalldAdapter.cs
@@ -0,0 +1,160 @@
+﻿using Renci.SshNet;
+using SettingLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirewallSettingSSHLib.FWAdapter
+{
+    /// <summary>
+    /// firewalld适配器
+    /// </summary>
+    public class FirewalldAdapter : FWAdapterBase
+    {
+        public override string Name 
+        {
+            get 
+            {
+                return "Firewalld";
+            }
+        }
+        /// <summary>
+        /// 检查是否运行中
+        /// </summary>
+        /// <param name="ssh"></param>
+        /// <returns></returns>
+        public override bool CheckEnable(SshClient ssh) 
+        {
+            SshCommand cmd = ssh.RunCommand("firewall-cmd --state");//查看当前规则
+            string res = cmd.Result;
+            if (!string.IsNullOrWhiteSpace(res)) 
+            {
+                res = res.Trim(' ', '\r', '\n');
+            }
+            return string.Equals(res, "running", StringComparison.CurrentCultureIgnoreCase);
+        }
+        /// <summary>
+        /// 加载现存规则
+        /// </summary>
+        /// <param name="dicPort"></param>
+        /// <returns></returns>
+        private  Dictionary<string, FirewallRule> LoadExists(SshClient ssh)
+        {
+            Dictionary<int, bool> dicPort = LoadRulePort();
+            SshCommand cmd = ssh.RunCommand("firewall-cmd --list-rich-rules");//查看当前规则
+            string res = cmd.Result;
+            
+            Dictionary<string, FirewallRule> dicExists = new Dictionary<string, FirewallRule>();
+            string line = null;
+            string sport = null;
+            string ip = null;
+            string protocol = null;
+            int port = 0;
+            using (StringReader reader = new StringReader(res))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    ip = SubValue("source address=", line);
+                    sport = SubValue("port port=", line);
+                    protocol = SubValue("protocol=", line);
+
+                    if (string.IsNullOrWhiteSpace(ip) || string.IsNullOrWhiteSpace(sport))
+                    {
+                        continue;
+                    }
+                    port = sport.ConvertTo<int>();
+                    if (!dicPort.ContainsKey(port)) //跳过非接管的端口
+                    {
+                        continue;
+                    }
+                    string key = GetKey(ip, port, protocol);
+                    dicExists[key] = new FirewallRule(ip, port, protocol);
+                }
+            }
+            return dicExists;
+        }
+
+        /// <summary>
+        /// 创建要执行的指令
+        /// </summary>
+        /// <param name="lstIP"></param>
+        /// <param name="dicExistsRule"></param>
+        public override List<string> CreateCommand(SshClient ssh)
+        {
+            Dictionary<string, FirewallRule> dicExistsRule = LoadExists(ssh);
+            List<string> lstIP = LoadUserIP();
+            List<FirewallRule> lstCreateItem = new List<FirewallRule>();//需要创建的列表
+            foreach (string ip in lstIP)
+            {
+                foreach (FirewallItem fwItem in _firewallRule)
+                {
+                    //int port = kvpPort.Key;
+                    string key = GetKey(ip, fwItem.Port, fwItem.Protocol);
+                    if (dicExistsRule.ContainsKey(key)) //已存在规则，在存在列表删除并跳过
+                    {
+                        dicExistsRule.Remove(key);
+                        continue;
+                    }
+                    lstCreateItem.Add(new FirewallRule(ip, fwItem.Port, fwItem.Protocol));
+                }
+            }
+
+            List<string> cmd = new List<string>();
+
+            foreach (FirewallRule rule in lstCreateItem)
+            {
+                cmd.Add(CreateAddCommand(rule));//增加白名单命令
+            }
+            foreach (KeyValuePair<string, FirewallRule> kvpRule in dicExistsRule)
+            {
+                FirewallRule rule = kvpRule.Value;
+                cmd.Add(CreateDeleteCommand(rule));//删除白名单命令
+            }
+            return cmd;
+        }
+
+        /// <summary>
+        /// 创建新增命令
+        /// </summary>
+        /// <returns></returns>
+        public string CreateAddCommand(FirewallRule rule)
+        {
+            StringBuilder sbCmd = new StringBuilder();
+            sbCmd.Append("firewall-cmd --permanent --add-rich-rule=\"rule family=\"ipv4\" source address=\"");
+            sbCmd.Append(rule.IP);
+            sbCmd.Append("\" port protocol=\"");
+            sbCmd.Append(rule.Protocol);
+            sbCmd.Append("\" port=\"");
+            sbCmd.Append(rule.Port.ToString());
+            sbCmd.Append("\" accept\"");
+            return sbCmd.ToString();
+        }
+        /// <summary>
+        /// 创建新增命令
+        /// </summary>
+        /// <returns></returns>
+        public string CreateDeleteCommand(FirewallRule rule)
+        {
+            StringBuilder sbCmd = new StringBuilder();
+            sbCmd.Append("firewall-cmd --permanent --remove-rich-rule=\"rule family=\"ipv4\" source address=\"");
+            sbCmd.Append(rule.IP);
+            sbCmd.Append("\" port protocol=\"");
+            sbCmd.Append(rule.Protocol);
+            sbCmd.Append("\" port=\""); ;
+            sbCmd.Append(rule.Port.ToString());
+            sbCmd.Append("\" accept\"");
+            return sbCmd.ToString();
+        }
+        /// <summary>
+        /// 重新加载防火墙
+        /// </summary>
+        /// <param name="ssh"></param>
+        public override void ReLoad(SshClient ssh)
+        {
+            ssh.RunCommand("firewall-cmd --reload");
+        }
+    }
+}
